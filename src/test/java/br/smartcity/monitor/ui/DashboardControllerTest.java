9339d5d7add0a7e929d73632a19dd9be39e3a7f5
BrunaDev <dev.bruna.magalhaes@gmail.com>
2026-09-04 11:14:01 -0300

    feat: integrate experiment execution flow

--- src/test/java/br/smartcity/monitor/ui/DashboardControllerTest.java
+++ src/test/java/br/smartcity/monitor/ui/DashboardControllerTest.java
@@ -11,7 +11,7 @@ class DashboardControllerTest {
     @Test
     void executaParaEConsolidaUmExperimentoReal() throws InterruptedException {
         DashboardController controller = new DashboardController();
-        controller.iniciar(new ConfiguracaoExperimento(1, 30, 100));
+        controller.iniciar(new ConfiguracaoExperimento(1, 100));
 
         Thread.sleep(550);
         DashboardSnapshot duranteExecucao = controller.obterSnapshot();
@@ -34,7 +34,7 @@ class DashboardControllerTest {
     @Test
     void resetLimpaLeiturasMasPreservaPossibilidadeDeNovaExecucao() throws InterruptedException {
         DashboardController controller = new DashboardController();
-        controller.iniciar(new ConfiguracaoExperimento(2, 10, 20));
+        controller.iniciar(new ConfiguracaoExperimento(2, 20));
         Thread.sleep(120);
         controller.parar();
         controller.resetar();
@@ -42,7 +42,7 @@ class DashboardControllerTest {
         assertEquals(DashboardController.Estado.PARADO, controller.getEstado());
         assertEquals(DashboardSnapshot.vazio(), controller.obterSnapshot());
 
-        controller.iniciar(new ConfiguracaoExperimento(4, 20, 10));
+        controller.iniciar(new ConfiguracaoExperimento(4, 10));
         assertEquals(4, controller.obterSnapshot().threadsAtivas());
         controller.parar();
     }
